761523fa9a2f13d8216f35d0deaddfeb389fffc3
Hanin Barakat <83194667+Octobar@users.noreply.github.com>
2021-11-19 17:52:02 +0300

    this is the translated python code

---
+++ convert.py
@@ -0,0 +1,31 @@
+print("Convert Weight:")
+
+print("Type 1 for Pounds to Kilograms and Grams")
+
+print("Type 2 for Kilograms to Pounds and Ounces")
+
+choice = input("What Conversion do you want?\n")
+
+choice = int(choice)
+
+if choice ==1:
+
+    weight_lbs=input("Converting from Pounds to Kilograms and Grams, please enter the weight in Pounds")
+    weight_lbs= int(weight_lbs)
+    new_kgs= float(weight_lbs*0.45359237) 
+    new_grams= float ((new_kgs%0.45359237)*1000 )
+
+    print("Converting " ,weight_lbs,
+"pound into Kilograms = ",new_kgs,"kg, which is ",new_grams," gram" )
+
+if choice ==2:
+
+    weight_kgs=input("Converting from Kilograms to Pounds and Ounces, please enter the weight in Kilograms")
+    weight_kgs= int(weight_kgs)
+
+    new_pound= float(weight_kgs*2.20462262)
+
+    new_ounce= float ((new_pound%16)*1000 )
+
+    print("Converting " ,weight_kgs,"pound into Kilograms = ",new_pound,"kg, which is ",new_ounce," gram" )
+
